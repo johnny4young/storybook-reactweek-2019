2e80a35d12daa798870a44b95f32a866ff9317a5
Johnny Iv Young Ospino <johnny.young@co-it004374.globant.com>
2019-03-05 15:57:36 -0500

    step 4: adding addons

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 
-import { storiesOf } from '@storybook/react';
+import {
+  storiesOf,
+  addParameters, addDecorator
+} from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import {
+  withKnobs,
+  text,
+  boolean,
+  number
+} from '@storybook/addon-knobs';
+import {
+  withA11y
+} from '@storybook/addon-a11y';
+
 
 import { Button, Welcome } from '@storybook/react/demo';
 
 //agregamos el componente que deseamo agregarle la historia
 import Tabs from '../Tabs';
+
+addDecorator(withKnobs);
+addDecorator(withA11y);
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
@@ -21,18 +37,22 @@ storiesOf('Button', module)
     </Button>
   ));
 storiesOf('Tabs', module)
-  .add('two tabs', () => { return (
+  .add('two tabs', () => (
     <Tabs tabsId = "First-Tab-Example" >
-      <div tabId = "Tab-1" label = "Tab 1" >
-      tab content 1
+      < div tabId = "Tab-1"
+      label = {
+        text('Tab Name1', 'Tab 1')
+      } >
+      {text('Tab Content 1','tab content 1')}
       </div>
       <div tabId = "Tab2" label = "Tab 2" >
         tab content 2
       </div>
     </Tabs>
-  )})
-  .add('three tabs', () => {
-    return (
+  ),
+  { notes: 'tabs con solo dos pestañas'})
+  .add('three tabs', () =>
+     (
       <Tabs tabsId = "First-Tab-Example" >
         <div tabId = "Tab-1" label = "Tab 1" >
           tab content 1
@@ -44,5 +64,6 @@ storiesOf('Tabs', module)
           tab content 3
         </div>
       </Tabs>
-    )
-  });
+    ),
+    { notes: 'tabs con solo 3 pestañas'}
+  );
